980837f72d11f9af286dc04b64ab4a9062f5d1cc
lijanahmmed <lijanahmmed@gmail.com>
2025-08-15 14:20:41 +0600

    added task15.js

---
+++ task15.js
@@ -0,0 +1,16 @@
+function ATM() {
+  let amount = 4000;
+  console.log("Welcome to PH Bank LTd");
+  console.log("Your Amount " + amount + " Taka");
+}
+
+function square(input) {
+  let squareNum = input * input;
+  return squareNum;
+}
+let result = square(49);
+console.log(result);
+
+
+// Output
+// 2401
