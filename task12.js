f2eb2dab8440c549af08e6242854187639073a13
lijanahmmed <lijanahmmed@gmail.com>
2025-08-15 14:10:56 +0600

    task12.js add

---
+++ task12.js
@@ -0,0 +1,20 @@
+function sum(input1, input2) {
+  let result = input1 + input2;
+    console.log(result);
+  return result;
+}
+function multiply(input1, input2) {
+  let result = input1 * input2;
+    console.log(result);
+  return result;
+}
+let a = 4;
+let b = 5;
+let result = sum(a, b) + multiply(a, b);
+console.log(result);
+
+
+// Output
+// 9
+// 20
+// 29
